0cedcb1cc9d0b945b5cee4b351ea02c68359f157
jojomo96 <48690914+jojomo96@users.noreply.github.com>
2025-01-05 17:15:38 +0100

    add ex03

---
+++ ex03/Bureaucrat.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <ostream>
+
+class Bureaucrat {
+	const std::string _name;
+	int _grade;
+
+public:
+	explicit Bureaucrat(std::string name, int grade);
+
+	Bureaucrat(Bureaucrat const &src);
+
+	~Bureaucrat();
+
+	Bureaucrat &operator=(Bureaucrat const &rhs);
+
+	[[nodiscard]] std::string getName() const;
+
+	[[nodiscard]] int getGrade() const;
+
+	void incrementGrade(int amount);
+
+	void decrementGrade(int amount);
+
+	class GradeTooHighException final : public std::exception {
+	public:
+		[[nodiscard]] const char *what() const noexcept override;
+	};
+
+	class GradeTooLowException final : public std::exception {
+	public:
+		[[nodiscard]] const char *what() const noexcept override;
+	};
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
